c09e0043ef28bdcd1e2b1dec00bda831514e5c38
Xuxiaoyu05 <34875319+Xuxiaoyu05@users.noreply.github.com>
2019-03-12 09:37:38 +0800

    Create QuickSort.py

---
+++ QuickSort.py
@@ -0,0 +1,31 @@
+# 快速排序
+
+# -*- coding:UTF-8 -*- 
+
+class Solution:
+	def quickSort(self, nums, left, right):
+		if left < right:
+			return
+		
+		pivot = nums[left]
+		i, j = left, right
+		
+		while i != j:
+			while nums[j] >= pivot and j >= left:
+				j -= 1
+			while nums[i] <= pivot and i <= right :
+		    	i += 1
+			
+			if i < j:
+				a[i], a[j] = a[j], a[i]
+		
+		nums[left] = a[i]
+		a[i] = pivot
+		
+		self.quickSort(nums, left, i-1)
+		self.quickSort(nums, i+1，right)
+
+
+nums = [4, 5, 3, 8, 2, 9, 6, 7, 1]
+S = Solution()
+print(S.quickSort(nums))
