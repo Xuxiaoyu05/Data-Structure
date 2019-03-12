08c5c143d757e96e7f777758e56de356e0851c02
Xuxiaoyu05 <34875319+Xuxiaoyu05@users.noreply.github.com>
2019-03-12 09:51:18 +0800

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -4,16 +4,16 @@
 
 class Solution:
 	def quickSort(self, nums, left, right):
-		if left < right:
+		if left > right:  # 此处别粗心
 			return
 		
 		pivot = nums[left]
 		i, j = left, right
 		
 		while i != j:
-			while nums[j] >= pivot and j >= left:
+			while nums[j] >= pivot and i < j:  # 注意：若j >= i, 则while循环停止，否则会一直交换
 				j -= 1
-			while nums[i] <= pivot and i <= right :
+			while nums[i] <= pivot and i < j:
 		    		i += 1
 			
 			if i < j:
@@ -24,8 +24,10 @@ class Solution:
 		
 		self.quickSort(nums, left, i-1)
 		self.quickSort(nums, i+1, right)
+		
+		return nums
 
 
 nums = [4, 5, 3, 8, 2, 9, 6, 7, 1]
 S = Solution()
-print(S.quickSort(nums))
+print(S.quickSort(nums, 0, len(nums)-1))
