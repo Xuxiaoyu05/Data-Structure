d77cc570e87e016885b4f2eaecc92dde7d96fdcc
Xuxiaoyu05 <34875319+Xuxiaoyu05@users.noreply.github.com>
2019-03-12 15:15:20 +0800

    Create HeapSort.py

---
+++ HeapSort.py
@@ -0,0 +1,19 @@
+# 堆排序
+
+# -*- coding:UTF-8 -*-
+
+class Solution:
+  def heapify(self, root, i):
+    left = 2 * i
+    right = 2* i + 1
+    
+    
+  
+  def BuildMaxHeap(self, root, i):
+    
+    
+    
+  def heapsort(self, nums):
+    # 构建最大堆
+    self.BuildMaxHeap(self)
+    
