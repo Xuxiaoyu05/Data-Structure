fb6f74b6016533e7b734301a4d0a3ac408207d05
Xuxiaoyu05 <34875319+Xuxiaoyu05@users.noreply.github.com>
2019-03-12 10:53:49 +0800

    Create InsertSort.py

---
+++ InsertSort.py
@@ -0,0 +1,26 @@
+# 插入排序
+
+# -*- coding: UTF-8 -*-
+
+class Solution:
+  def insertSort(self, nums):
+  
+    length = len(nums)
+      
+    for i in range(1, length):
+      itemToInsert = nums[i]
+      position = i
+      while position >= 1 and nums[position-1] > itemToInsert:  # 之前的都是排好序的，因此将前面的元素与itemToInsert比较
+      	nums[position] = nums[position-1]
+		position -= 1
+	  nums[position] = itemToInsert
+	
+	return nums
+
+
+
+nums = [4, 5, 3, 8, 2, 9, 6, 7, 1]
+
+S = Solution()
+
+print(S.insertSort(nums))
